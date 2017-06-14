ddb2a15797655603e136d832a082c357be6b4d92
bradys <https://bradys@github.com>
2017-06-14 13:31:29 +0100

    Added handler logic to do_POST(), added stubs and TODO's to unfinished flows

--- var/www/cgi-bin/Server.py
+++ var/www/cgi-bin/Server.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-import cgi
 import csv
+import cgi
 import os
-from http.server import CGIHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
+
+'''
+Server settings
+'''
 HOST_NAME = '' # Change to webserver host
 PORT_NUMBER = 8080 # Change to 80 on production server
 
-form = cgi.FieldStorage()
-enteredName =  form.getvalue('name')
-enteredPass =  form.getvalue('password')
+'''
+Configure cgi
+'''
 cgi_dir = "s1811185\WebAppDev\\"
 
 '''
@@ -18,11 +22,44 @@ Handler class subclassing CGIHTTPRequestHandler to define how the POST request i
 CGIHTTPRequestHandler can be enabled in the command line by passing the --cgi option:
 e.g. python -m http.server --cgi 8000
 '''
-class user_handler_class(CGIHTTPRequestHandler):
-	def do_POST(self):
+class user_handler_class(BaseHTTPRequestHandler):
+	def do_POST(self):		
 		print( "incoming POST request from: ", self.client_address )
+		#cgi handles GET requests by default, so need to explicitly set to POST and pass in the do_POST's rfile and headers
+		postData = cgi.FieldStorage(
+									fp=self.rfile,
+									headers=self.headers,
+									environ={'REQUEST_METHOD':'POST'})
+		print(postData)
 
-		
+		#A name and password should be attached to every POST request so server knows where the request is coming from. 
+		if "name" in postData and "password" in postData:
+			clientName =  postData.getvalue('name')
+			clientPass =  postData.getvalue('password')
+
+			with open(cgi_dir + 'User_Info.csv', newline='') as csvfile:
+				reader = csv.DictReader(csvfile)
+				for row in reader:
+					name = row["name"]
+					password = row["pwd"]
+					jsonFileLocation = row['a']
+					if clientName == name:
+						if clientPass == password:
+							userJsonFileLocation = jsonFileLocation
+					else:
+						break
+						#TODO username and pass not found, add them to the csv instead of breaking out
+
+			if "data" in postData:
+				#TODO If data is included then this is a save and upload action and not a login, so save the data to the userJsonFile add the data to the editor page and return the editor page.
+				pass
+			else:
+				#TODO No data means this is a login return the editor page and load up jsonFile using the jsonFileLocation.
+				pass
+
+		else:
+			pass
+			#TODO this needs to return a 404 page instea of pass.
 
 '''
 run function to create and start http server instance httpd
